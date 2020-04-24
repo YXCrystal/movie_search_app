1abc0ae0d43d10b4b66c0ef7d52c834974213b4f
Crystal <yxcwuu@gmail.com>
2020-04-24 15:20:24 -0400

    Install packages

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var request = require("request");
+var bodyParser = require("body-parser");
 var app = express();
+require("dotenv").config();
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    res.render("homepage");
+});
+
+request(`http://www.omdbapi.com/?s=star&apikey=${process.env.APIKEY}`, function(err, res, body) {
+    if (!err & res.statusCode === 200) {
+        console.log(body);
+    } else {
+        console.log("error", err)
+    }
 });
 
 app.listen(3000, function() {
