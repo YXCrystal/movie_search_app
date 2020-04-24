cfb3ac45b771a4169de6aa55ea5cfd4f39d41adf
Crystal <yxcwuu@gmail.com>
2020-04-24 16:13:24 -0400

    Add API request

--- app.js
+++ app.js
@@ -9,17 +9,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.get("/homepage", function(req, res) {
     res.render("homepage");
 });
 
-request(`http://www.omdbapi.com/?s=star&apikey=${process.env.APIKEY}`, function(err, res, body) {
-    if (!err & res.statusCode === 200) {
-        console.log(body);
-    } else {
-        console.log("error", err)
-    }
+app.post("/homepage", function(req, res) {
+    var query = req.body.movie
+    request(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.APIKEY}`, function(err, response, body) {
+        if (!err & response.statusCode === 200) {
+            var parsedData = JSON.parse(body);
+            var movies = parsedData.Search;
+            res.render("movies", {movies, query})
+        } else {
+            console.log("error", err)
+        }
+    });
 });
+
+
 
 app.listen(3000, function() {
     console.log("Server has started")
